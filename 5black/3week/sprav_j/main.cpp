38d91e8e5253dfaf0389c77ed4502c5edb966c98
Andrew Ozmitel <andrew@ozmitel.com>
2021-04-30 01:55:23 +0300

    Remove some stub code

--- 5black/3week/sprav_j/main.cpp
+++ 5black/3week/sprav_j/main.cpp
@@ -12,9 +12,6 @@ int main() {
 
   SpravPtr sprav = make_shared<Sprav>();
   SpravIO sprav_io(sprav, cout, Format::JSON_PRETTY);
-
-  // ifstream input("example2.in.json");
-  // sprav_io.Process(input);
   sprav_io.Process(cin);
 
   return 0;
