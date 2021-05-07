6092e28ba0f431b303bc0465d2e485cd38869a71
Andrew Ozmitel <andrew@ozmitel.com>
2021-05-07 21:26:48 +0300

    Add lat&lon for stops in DB

--- 5black/5week/sprav_q/stop.cpp
+++ 5black/5week/sprav_q/stop.cpp
@@ -27,6 +27,8 @@ int Stop::DistanceTo(size_t other) const {
 
 void Stop::SerializeTo(SpravSerialize::Stop& m) const {
   m.set_id(id);
+  m.set_latitude(lat);
+  m.set_longitude(lon);
   m.mutable_name()->assign(name);
   for (auto bus : buses) {
     m.mutable_bus()->Add(bus);
@@ -36,6 +38,8 @@ void Stop::SerializeTo(SpravSerialize::Stop& m) const {
 Stop Stop::Parse(const SpravSerialize::Stop& m) {
   Stop s;
   s.id = m.id();
+  s.lat = m.latitude();
+  s.lon = m.longitude();
   s.buses.insert(m.bus().begin(), m.bus().end());
 
   return s;
