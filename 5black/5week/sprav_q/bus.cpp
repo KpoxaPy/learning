54ef2cc081860634f0be2d1b42964983800d48ba
Andrew Ozmitel <andrew@ozmitel.com>
2021-05-08 05:01:02 +0300

    Add bus stops serialization

--- 5black/5week/sprav_q/bus.cpp
+++ 5black/5week/sprav_q/bus.cpp
@@ -5,6 +5,10 @@ using namespace std;
 void Bus::SerializeTo(SpravSerialize::Bus& m) const {
   m.set_id(id);
   m.mutable_name()->assign(name);
+  for (auto stop_id : stops) {
+    m.add_stop_id(stop_id);
+  }
+  m.set_is_roundtrip(is_roundtrip);
 
   m.set_stops_count(stops_count);
   m.set_unique_stops_count(unique_stops_count);
@@ -16,6 +20,11 @@ Bus Bus::Parse(const SpravSerialize::Bus& m) {
   Bus b;
 
   b.id = m.id();
+  for (auto stop_id: m.stop_id()) {
+    b.stops.push_back(stop_id);
+  }
+  b.is_roundtrip = m.is_roundtrip();
+
   b.stops_count = m.stops_count();
   b.unique_stops_count = m.unique_stops_count();
   b.length = m.length();
