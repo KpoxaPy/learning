0c333ad7f5c0c886533dae48a88d1defb02ff2df
Andrew Ozmitel <andrew@ozmitel.com>
2021-05-09 00:53:14 +0300

    Add json parsing checking for yellow pages db

--- 5black/5week/sprav_r/pages.cpp
+++ 5black/5week/sprav_r/pages.cpp
@@ -3,14 +3,33 @@
 #include <google/protobuf/util/json_util.h>
 
 #include <sstream>
+#include <iomanip>
+#include <stdexcept>
 
 using namespace std;
+using ::google::protobuf::util::JsonStringToMessage;
+using ::google::protobuf::util::JsonParseOptions;
+using ::google::protobuf::util::Status;
+
+namespace {
+
+const JsonParseOptions JSON_PARSE_OPTIONS = [](){
+  JsonParseOptions o;
+  o.ignore_unknown_fields = false;
+  return o;
+}();
+
+} // namespace
 
 Pages::Pages(const Json::Dict& dict) {
   YellowPages::Database db;
   ostringstream ss;
   Json::PrintNode(dict, ss);
-  ::google::protobuf::util::JsonStringToMessage(ss.str(), &db);
+  if (auto status = JsonStringToMessage(ss.str(), &db, JSON_PARSE_OPTIONS); status != Status::OK) {
+    ostringstream err_ss;
+    err_ss << "Failed to parse " << quoted(ss.str()) << " as yellow pages database: " << status.ToString();
+    throw runtime_error(err_ss.str());
+  }
   ParseFrom(db);
 }
 
