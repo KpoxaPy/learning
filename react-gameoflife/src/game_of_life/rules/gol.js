6ed6b0da8eea208f307f38c596b4bd0ae8f94632
Andrew Ozmitel <andrew@ozmitel.com>
2023-05-30 22:06:05 +0600

    Move rules logic to its own class
    Signed-off-by: Andrew Ozmitel <andrew@ozmitel.com>

--- react-gameoflife/src/game_of_life/rules/gol.js
+++ react-gameoflife/src/game_of_life/rules/gol.js
@@ -1,5 +1,24 @@
 class GoL_Rules {
+  iterate(value, neighborsFunc) {
+    const neighbors = this.calcAliveNeighbors(neighborsFunc);
 
+    if (value) {
+      return neighbors === 2 || neighbors === 3 ? 1 : 0;
+    }
+    return neighbors === 3 ? 1 : 0;
+  }
+
+
+  calcAliveNeighbors(neighborsFunc) {
+    return neighborsFunc(-1, -1)
+      + neighborsFunc(-1, 0)
+      + neighborsFunc(-1, 1)
+      + neighborsFunc(0, -1)
+      + neighborsFunc(0, 1)
+      + neighborsFunc(1, -1)
+      + neighborsFunc(1, 0)
+      + neighborsFunc(1, 1);
+  }
 };
 
 export default GoL_Rules;
